a1939d3270ae894d96f105dd0a6e3494f32270e7
Adam Crume <acrume@google.com>
2017-07-29 10:36:57 -0700

    Update the documentation

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
-implementors["libc"] = [];implementors["tensorflow"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='tensorflow/struct.Status.html' title='tensorflow::Status'>Status</a>",];
+implementors["libc"] = [];
+implementors["tensorflow"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"tensorflow/struct.Status.html\" title=\"struct tensorflow::Status\">Status</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
