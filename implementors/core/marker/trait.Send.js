7475522cd132b76f22084f1be1f929fb1fab29a4
Adam Crume <acrume@google.com>
2018-03-25 17:13:14 -0700

    Update the documentation

--- implementors/core/marker/trait.Send.js
+++ implementors/core/marker/trait.Send.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["tensorflow"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"tensorflow/struct.Operation.html\" title=\"struct tensorflow::Operation\">Operation</a>","impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"tensorflow/struct.TensorDataCRepr.html\" title=\"struct tensorflow::TensorDataCRepr\">TensorDataCRepr</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"tensorflow/trait.TensorType.html\" title=\"trait tensorflow::TensorType\">TensorType</a>,&nbsp;</span>",];
+implementors["tensorflow"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"tensorflow/struct.Operation.html\" title=\"struct tensorflow::Operation\">Operation</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
